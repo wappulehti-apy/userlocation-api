aca6a063f5e7c9d50fe799ca366a90d7df542338
Pietu <pietu.roisko@aalto.fi>
2019-04-03 14:50:27 +0300

    rename var

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     # From ENV variables
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY") or "this-really-needs-to-be-changed"
     BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
-    BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_PASSWORD")
+    BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
     GEO_NAME = os.getenv("GEO_NAME") or 'default'

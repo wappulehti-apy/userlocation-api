446584b3f2d05e31be01bb8ee0d442fd4bb99cf9
Pietu <pietu.roisko@aalto.fi>
2019-04-03 14:36:52 +0300

    tweak config

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv(verbose=True)
 class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY") or "this-really-needs-to-be-changed"
     BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
     BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_PASSWORD")
-    API_VERSION = '1.0'
+    GEO_NAME = os.getenv("BASIC_AUTH_PASSWORD")
+    API_VERSION = "1.0"

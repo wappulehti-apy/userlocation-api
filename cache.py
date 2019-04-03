1a7c447946e7de8c82d822cfeb11044d59c6d755
Pietu <pietu.roisko@aalto.fi>
2019-04-04 00:10:16 +0300

    try with bmemcache

--- cache.py
+++ cache.py
@@ -1,23 +1,20 @@
+import bmemcached
 import os
-from flask_caching import Cache
 
 
 def create_cache(app):
-    # From https://devcenter.heroku.com/articles/memcachier
-    cache = Cache()
-    cache_servers = os.environ.get('MEMCACHIER_SERVERS')
-    app.logger.info('memcachier servers: %s', cache_servers)
-    if cache_servers is None:
+
+    servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
+    user = os.environ.get('MEMCACHIER_USERNAME', '')
+    passw = os.environ.get('MEMCACHIER_PASSWORD', '')
+    if servers is None:
+        from flask_caching import Cache
+        cache = Cache() 
         # Fall back to simple in memory cache (development)
         cache.init_app(app, config={'CACHE_TYPE': 'simple'})
-    else:
-        cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-        cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-        app.logger.info('username {}'.format(cache_user))
-        cache.init_app(app,
-                       config={'CACHE_TYPE': 'saslmemcached',
-                               'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-                               'CACHE_MEMCACHED_USERNAME': cache_user,
-                               'CACHE_MEMCACHED_PASSWORD': cache_pass})
+        return cache
 
+    cache = bmemcached.Client(servers, username=user, password=passw)
+
+    cache.enable_retry_delay(True)  # Enabled by default. Sets retry delay to 5s.
     return cache

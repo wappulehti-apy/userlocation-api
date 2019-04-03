e31092886bf47c70046be89fe8cc613580f7548f
Pietu <pietu.roisko@aalto.fi>
2019-04-03 16:13:36 +0300

    restructure config

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
+import sys
 from flask import Flask, jsonify
 from flask_basicauth import BasicAuth
 
-app = Flask(__name__)
-app.config.from_object('config.Config')
 
-basic_auth = BasicAuth(app)
+def create_app():
+    app = Flask(__name__)
+    if "pytest" in sys.modules:
+        app.config.from_object('config.Testing')
+    else:
+        app.config.from_object('config.Config')
+
+    basic_auth = BasicAuth(app)
+
+    return app
 
 
 def geojson(latitude, longitude, name):
@@ -20,6 +29,7 @@ def geojson(latitude, longitude, name):
     }
 
 
+app = create_app()
 @app.route('/')
 def get_cooridnates():
     response = geojson(0, 0, app.config['GEO_NAME'])
--- config.py
+++ config.py
@@ -1,16 +1,24 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
-
 
 class Config(object):
     DEBUG = False
+    TESTING = False
     CSRF_ENABLED = True
     BASIC_AUTH_FORCE = True
 
     # From ENV variables
+    load_dotenv(verbose=True)
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY") or "this-really-needs-to-be-changed"
     BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
     BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
     GEO_NAME = os.getenv("GEO_NAME") or 'default'
+
+
+class Testing(Config):
+    TESTING = True
+    SECRET_KEY = "mysecretley"
+    BASIC_AUTH_USERNAME = "myusername"
+    BASIC_AUTH_PASSWORD = "mypassword"
+    GEO_NAME = "myname"

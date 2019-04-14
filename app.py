27bee3338a48e6d2bcf12704cf37eae9fd345efd
Pietu <pietu.roisko@aalto.fi>
2019-04-14 19:09:03 +0300

    fix config loading

--- app.py
+++ app.py
@@ -15,8 +15,10 @@ from models import Location
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(os.environ['APP_SETTINGS'])
-    app.logger.info(f'Loaded APP_SETTINGS from {os.environ["APP_SETTINGS"]}')
+    app.logger.setLevel(logging.INFO)
+    settings_class = os.getenv('APP_SETTINGS', 'config.Config')
+    app.config.from_object(settings_class)
+    app.logger.info(f'Loaded APP_SETTINGS from {settings_class}')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.logger.info(f'Connecting to DB at {app.config["SQLALCHEMY_DATABASE_URI"]}')
     db.init_app(app)

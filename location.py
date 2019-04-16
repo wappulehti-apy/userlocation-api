fd16da8220903adc5457594ea21945ae08b171f8
Pietu <pietu.roisko@aalto.fi>
2019-04-16 22:29:00 +0300

    implement long polling

--- location.py
+++ location.py
@@ -36,14 +36,32 @@ def valid_phone(phone):
 def requestcall():
     data = request.get_json()
     phone = data.get('phoneNumber')
+    public_id = data.get('sellerId')
+    buyer_id = '123'  # TODO
+
+    if phone is None or public_id is None:
+        return jsonify({'error': True, 'message': 'you must specify phoneNumber and sellerId'}), 400
+    if not valid_phone(phone):
+        return jsonify({'error': True, 'message': 'phoneNumber is not valid'}), 400
+
+    user = Location.query.filter_by(public_id=public_id).first()
+    if user is None:
+        return jsonify({'error': True, 'message': 'no user found'}), 404
+
     webhook = Webhook(app.config['WEBHOOK_URL'])
 
     app.logger.info('sending contact request')
-    if webhook.send_contact_request(user.id, buyer_id, phone):
-        return jsonify({'success': True})
-    else:
+    if not webhook.send_contact_request(user.id, buyer_id, phone):
+        app.logger.info('contact request failed')
         return jsonify({'success': False})
-
+    end_time = datetime.now() + timedelta(seconds=60)
+    while True:
+        if datetime.now() >= end_time:
+            return jsonify({'success': False})
+        sleep(5)
+        response = redis.get(f'response:{buyer_id}')
+        if response is not None:
+            return jsonify({'success': True})
 
 
 @location.route('/')

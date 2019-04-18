59303f9e90977291bf051768e84b6fca5e8c75ba
Pietu <pietu.roisko@aalto.fi>
2019-04-18 22:52:36 +0300

    test expiration

--- test_redismap.py
+++ test_redismap.py
@@ -6,7 +6,7 @@ from redismap import RedisMap
 
 @pytest.fixture(scope='function')
 def redis_map(redis_conn):
-    yield RedisMap(None, redis_conn)
+    yield RedisMap(MagicMock(), redis_conn)
     redis_conn.flushdb()
 
 
@@ -17,7 +17,7 @@ def redis_mock(redis_conn):
 
 @pytest.fixture(scope='function')
 def redis_map_mock(redis_mock):
-    yield RedisMap(None, redis_mock)
+    yield RedisMap(MagicMock(), redis_mock)
 
 
 def test_add_user_uses_setex(redis_mock, redis_map_mock):
@@ -30,3 +30,14 @@ def test_update_user_location_uses_geoadd(redis_mock, redis_map_mock):
     redis_map_mock.update_user_location(999, 60.0, 24.0, initials='A A')
 
     redis_mock.geoadd.assert_called_with('loc', 60.0, 24.0, 'Q2k23')
+
+
+def test_expire_locations_removes_user(redis_mock, redis_map_mock):
+    zscan_iter = [[('1krgZy', 3716815713752047.0), ('3eA43', 3716815713752047.0)]]
+    exists = side_eddects = [1, 0]  # returns 1, then 0
+    redis_mock.exists.side_effect = exists
+    redis_mock.zscan_iter.side_effect = zscan_iter
+
+    redis_map_mock.expire_locations()
+
+    redis_mock.zrem.assert_called_with('loc', '3eA43')

e45041c4c3502e39a9dae14ee0fe529ba5b12e5c
Pietu R <pietu_r@gmx.com>
2019-12-30 21:19:19 +0200

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# GPS map for multiple users
+
+Simple webapp for setting and getting locations based on user id's.
+
+## Getting started
+# Configuration
+The following environment variables are available:
+- `BASIC_AUTH_USERNAME` specifies username for HTTP basic auth
+- `BASIC_AUTH_PASSWORD` specifies password for HTTP basic auth
+
+
+## Development
+
+Install dev-dependencies
+```bash
+pipenv install --dev
+```
+
+## Contributing
+
+Lint your code (PEP8)
+```bash
+pipenv run lint
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+
+class Config(object):
+    """Base config. Used in Production."""
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+
+    # From ENV variables
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY") or "this-really-needs-to-be-changed"
+    BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
+    BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
+    WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+    REDIS_URL = os.getenv("REDIS_URL")
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "").split(",")
+
+
+class Develop(Config):
+    from dotenv import load_dotenv
+    load_dotenv(verbose=True)
+    REDIS_URL = "redis://redis"
+
+
+class Testing(Config):
+    DEBUG = True
+    TESTING = True
+    SECRET_KEY = "mysecretkey"
+    BASIC_AUTH_USERNAME = "myusername"
+    BASIC_AUTH_PASSWORD = "mypassword"

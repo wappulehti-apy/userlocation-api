6ed7589589d99357b052e37ded8b8124be9ace09
Pietu R <pietu_r@gmx.com>
2020-01-01 18:07:35 +0200

    implement message enpoint (post)

--- api/message.py
+++ api/message.py
@@ -1,9 +1,12 @@
+import re
 from api.auth import basic_auth
 from flask import current_app as app
-from flask import request
+from flask import request, abort
 from flask_restful import Resource, reqparse
 from werkzeug.exceptions import HTTPException
 from functools import wraps
+from webhook import webhook
+from flask import current_app as app
 
 
 def validate_client_id(f):
@@ -18,6 +21,26 @@ def validate_client_id(f):
         return f(*args, **kwargs)
 
     return wrapped
+
+
+def valid_phone_number(phone):
+    if len(phone) < 6 or len(phone) > 15:
+        return False
+    # Must contain at least 5 numbers
+    if not re.search(r"\d\d.*\d\d\d", phone):
+        return False
+    # Should only contain these characters
+    if re.search(r"[^ 0-9\+\-\(\)]", phone):
+        return False
+    return True
+
+
+def validate_args(args):
+    """Validate client message. We expect message to be a plain phone number."""
+    if not valid_phone_number(args.message):
+        abort(400, description='invalid message: must be a valid phone number of format "(+)123 456789')
+    if len(args.user_id) < 5:
+        abort(400, description='invalid user_id')
 
 
 class Message(Resource):
@@ -37,3 +60,17 @@ class Message(Resource):
         app.redis.delete(f'response:{client_id}')
 
         return {'response': response}
+
+    def post(self):
+        client_id = request.headers.get('clientId')
+        validate_client_id(client_id)
+
+        args = self.parser.parse_args()
+        validate_args(args)
+
+        app.logger.info('send message from "%s" to "%s" (%s)', client_id, args.user_id, request.remote_addr)
+
+        if not webhook.send(client_id=client_id, user_id=args.user_id, message=args.message):
+            app.logger.error('message webhook failed')
+            return {'success': False, 'error': True}, 500
+        return {'success': True}
--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,32 @@
 from flask import Flask
 from flask import current_app as app
 from flask_restful import Resource, Api
+from werkzeug.exceptions import HTTPException
+
+
+class ExtendedApi(Api):
+    def handle_error(self, err):
+        """Extended error response formatting."""
+        app.logger.error(err)
+        response = {'error': True, 'message': 'unknown error'}
+        if isinstance(err, HTTPException):
+            if 'description' in err.__dict__:
+                app.logger.error('desc')
+                response['message'] = err.__dict__.get('description')
+            elif 'data' in err.__dict__:
+                app.logger.error('data')
+                msg = err.data.get('message')
+                if (isinstance(msg, dict)):
+                    response['message'] = ','.join([f'invalid {k}: {v}' for k, v in msg.items()])
+                else:
+                    response['message'] = err.data.get('message')
+        else:
+            response['message'] = str(err)
+        return response, err.code
 
 
 def init_app(app):
-    api = Api(app)
+    api = ExtendedApi(app)
     from . import location, message
     api.add_resource(location.LocationList, '/locations')
     api.add_resource(location.Location, '/locations/<int:user_id>')
